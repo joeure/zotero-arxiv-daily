f28962bde60c0640bb43f2b0bbce6faa4881bda2
joeure <124420184+joeure@users.noreply.github.com>
2026-04-11 09:29:08 +0800

    Update local.py
    Update local encoder

--- src/zotero_arxiv_daily/reranker/local.py
+++ src/zotero_arxiv_daily/reranker/local.py
@@ -2,14 +2,17 @@ from .base import BaseReranker, register_reranker
 import logging
 import warnings
 import numpy as np
+
+
 @register_reranker("local")
 class LocalReranker(BaseReranker):
     def get_similarity_score(self, s1: list[str], s2: list[str]) -> np.ndarray:
         from sentence_transformers import SentenceTransformer
+
         if not self.config.executor.debug:
             from transformers.utils import logging as transformers_logging
             from huggingface_hub.utils import logging as hf_logging
-    
+
             transformers_logging.set_verbosity_error()
             hf_logging.set_verbosity_error()
             logging.getLogger("sentence_transformers").setLevel(logging.ERROR)
@@ -19,12 +22,17 @@ class LocalReranker(BaseReranker):
             logging.getLogger("huggingface_hub.utils._http").setLevel(logging.ERROR)
             warnings.filterwarnings("ignore", category=FutureWarning)
 
-        encoder = SentenceTransformer(self.config.reranker.local.model, trust_remote_code=True)
-        if self.config.reranker.local.encode_kwargs:
-            encode_kwargs = self.config.reranker.local.encode_kwargs
-        else:
-            encode_kwargs = {}
-        s1_feature = encoder.encode(s1,**encode_kwargs,show_progress_bar=True)
-        s2_feature = encoder.encode(s2,**encode_kwargs,show_progress_bar=True)
+        model_name = self.config.reranker.local.model
+        trust_remote_code = getattr(self.config.reranker.local, "trust_remote_code", False)
+
+        encoder = SentenceTransformer(
+            model_name,
+            trust_remote_code=trust_remote_code,
+        )
+
+        encode_kwargs = self.config.reranker.local.encode_kwargs or {}
+
+        s1_feature = encoder.encode(s1, **encode_kwargs, show_progress_bar=True)
+        s2_feature = encoder.encode(s2, **encode_kwargs, show_progress_bar=True)
         sim = encoder.similarity(s1_feature, s2_feature)
         return sim.numpy()
